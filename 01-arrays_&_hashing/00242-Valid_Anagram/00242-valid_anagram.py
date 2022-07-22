1bde00e86de3ed4867412fbb166119c28e7dbfa9
poseidon-code <pritamhalder05062000@gmail.com>
2022-07-22 22:05:05 +0530

    add: python: 1-242

---
+++ 01-arrays_&_hashing/00242-Valid_Anagram/00242-valid_anagram.py
@@ -0,0 +1,33 @@
+# 242: Valid Anagram
+# https://leetcode.com/problems/valid-anagram/
+
+from itertools import count
+
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t) : return False
+
+        n = len(s)
+        counts = [0]*26
+
+        for i in range(n):
+            counts[ord(s[i]) - ord('a')]+=1
+            counts[ord(t[i]) - ord('a')]-=1
+
+        for i in range(26):
+            if counts[i] != 0 : return False
+        
+        return True
+
+
+if __name__ == "__main__":
+    o = Solution()
+
+    # INPUT
+    s = "rat"
+    t = "cat"
+
+    # OUTPUT
+    result = o.isAnagram(s, t)
+    print(result)
