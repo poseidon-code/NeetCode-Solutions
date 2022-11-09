ce812166423389b40f8c49923be00dbcab2ad798
Pritam Halder <61552390+poseidon-code@users.noreply.github.com>
2022-11-09 10:00:00 +0530

    add: java: 6-206

---
+++ 06-linked_list/00206-Reverse_Linked_List/00206-reverse_linked_list.java
@@ -0,0 +1,61 @@
+// 206: Reverse Linked List
+// https://leetcode.com/problems/reverse-linked-list/
+
+
+class Solution {
+    public static ListNode head = null;
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    public static void llInput(int[] inputs) {
+        for (int i=inputs.length-1; i>=0; i--) {
+            ListNode node = new ListNode(inputs[i], head);
+            head = node;
+        }
+    }
+
+    public static void llOutput(ListNode head) {
+        while (head != null) {
+            System.out.print(head.val + " -> ");
+            head = head.next;
+        }
+        System.out.println("NULL");
+    }
+
+    // SOLUTION
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) return head;
+
+        ListNode previous = null;
+        ListNode current = head;
+        ListNode next = head.next;
+
+        while (current != null) {
+            next = current.next;
+            current.next = previous;
+            previous = current;
+            current = next;
+        }
+
+        return previous;
+    }
+
+
+    public static void main(String[] args) {
+        Solution o = new Solution();
+
+        // INPUT
+        int[] li = {1,2,3,4,5};
+        llInput(li);
+
+        // OUTPUT
+        var result = o.reverseList(head);
+        llOutput(result);
+    }
+}
