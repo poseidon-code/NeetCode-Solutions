171cc8634bffa9a8b26fe2b0a401482fb6b0b2ba
Pritam Halder <61552390+poseidon-code@users.noreply.github.com>
2022-11-14 22:58:15 +0530

    add: go: 6-21

---
+++ 06-linked_list/00021-Merge_Two_Sorted_Lists/00021-merge_two_sorted_lists.go
@@ -0,0 +1,77 @@
+// 21: Merge Two Sorted Lists
+// https://leetcode.com/problems/merge-two-sorted-lists/
+
+package main
+
+import "fmt"
+
+type ListNode struct {
+    Val int
+    Next *ListNode
+}
+
+type LinkedList struct {
+    head *ListNode
+}
+
+func llInput(head **ListNode, inputs []int) {
+    for i:=len(inputs)-1; i>=0; i-- {
+        node := &ListNode{inputs[i], *head}
+        *head = node
+    }
+}
+
+func llOutput(head *ListNode) {
+    for head != nil {
+        fmt.Print(head.Val, " -> ")
+        head = head.Next
+    }
+    fmt.Println("NULL")
+}
+
+// SOLUTION
+func mergeTwoLists(list1, list2 *ListNode) *ListNode {
+    if list1 == nil {return list2}
+    if list2 == nil {return list1}
+
+    p := list1
+    if (list1.Val > list2.Val) {
+        p = list2
+        list2 = list2.Next
+    } else {
+        list1 = list1.Next
+    }
+
+    c := p
+    for list1!=nil && list2!=nil {
+        if (list1.Val < list2.Val) {
+            c.Next = list1
+            list1 = list1.Next
+        } else {
+            c.Next = list2
+            list2 = list2.Next
+        }
+        c = c.Next
+    }
+
+    if list1 == nil {
+        c.Next = list2
+    } else {
+        c.Next = list1
+    }
+
+    return p
+}
+
+func main() {
+    head1, head2 := LinkedList{}, LinkedList{}
+    
+    // INPUT
+    li1, li2 := []int{1,2,4}, []int{1,3,4}
+    llInput(&head1.head, li1)
+    llInput(&head2.head, li2)
+
+    // OUTPUT
+    result := mergeTwoLists(head1.head, head2.head)
+    llOutput(result)
+}
