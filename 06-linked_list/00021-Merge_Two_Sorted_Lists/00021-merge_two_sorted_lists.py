84e8566cd31e0cf93038a0274205906d6084ff9f
Pritam Halder <61552390+poseidon-code@users.noreply.github.com>
2022-11-15 20:49:42 +0530

    add: python: 6-21

---
+++ 06-linked_list/00021-Merge_Two_Sorted_Lists/00021-merge_two_sorted_lists.py
@@ -0,0 +1,72 @@
+# 21: Merge Two Sorted Lists
+# https://leetcode.com/problems/merge-two-sorted-lists/
+
+
+class ListNode:
+    def __init__(self, x: int=None):
+        self.val = x
+        self.next = None
+
+class LinkedList:
+    def __init__(self) -> None:
+        self.head = None
+
+    def Input(self, inputs: list[int]):
+        for i in inputs:
+            node = ListNode(i)
+            if self.head:
+                tail = self.head
+                while tail.next != None: tail = tail.next
+                tail.next = node
+            else:
+                self.head = node
+
+    def Output(self, head: ListNode):
+        while (head != None):
+            print(head.val, "-> ", end='')
+            head = head.next
+        print("NULL")
+
+
+class Solution:
+    def mergeTwoLists(self, list1: ListNode, list2: ListNode) -> ListNode:
+        if list1 == None: return list2
+        if list2 == None: return list1
+
+        p = list1
+        if list1.val > list2.val:
+            p = list2
+            list2 = list2.next
+        else:
+            list1 = list1.next
+
+        c = p
+        while list1!=None and list2!=None:
+            if list1.val < list2.val:
+                c.next = list1
+                list1 = list1.next
+            else:
+                c.next = list2
+                list2 = list2.next
+            c = c.next
+
+        if list1 == None:
+            c.next = list2
+        else:
+            c.next = list1
+
+        return p
+
+
+if __name__ == "__main__":
+    o = Solution()
+    head1, head2 = LinkedList(), LinkedList()
+
+    # INPUT
+    li1, li2 = [1,2,4], [1,3,4]
+    head1.Input(li1)
+    head2.Input(li2)
+
+    # OUTPUT
+    result = o.mergeTwoLists(head1.head, head2.head)
+    head1.Output(result)
