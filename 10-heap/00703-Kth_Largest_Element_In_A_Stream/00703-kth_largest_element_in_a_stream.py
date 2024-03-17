5aba576ab3cbfbff1d70f2ea773ffab614047504
Pritam Halder <61552390+poseidon-code@users.noreply.github.com>
2024-03-17 10:00:00 +0530

    fix: python: 10-703

--- 10-heap/00703-Kth_Largest_Element_In_A_Stream/00703-kth_largest_element_in_a_stream.py
+++ 10-heap/00703-Kth_Largest_Element_In_A_Stream/00703-kth_largest_element_in_a_stream.py
@@ -1,3 +1,38 @@
 # 703: Kth Largest Element In A Stream
 # https://leetcode.com/problems/kth-largest-element-in-a-stream
 
+
+import heapq
+
+
+class KthLargest:
+    def __init__(self, k: int, nums: list[int]) -> None:
+        self.pq = nums
+        self.size = k
+        
+        heapq.heapify(self.pq)
+        while len(self.pq) > k:
+            heapq.heappop(self.pq)
+
+    def add(self, val: int) -> int:
+        if len(self.pq) < self.size:
+            heapq.heappush(self.pq, val)
+        elif val > self.pq[0]:
+            heapq.heapreplace(self.pq, val)
+        return self.pq[0]
+
+
+
+if __name__ == "__main__":
+
+    # INPUT
+    k = 3
+    nums = [4,5,8,2]
+    o = KthLargest(k, nums)
+
+    # OPERATIONS
+    print(o.add(3))
+    print(o.add(5))
+    print(o.add(10))
+    print(o.add(9))
+    print(o.add(4))
